4a45f307c4c3da8c43de6dde13b3b1b23b40a81d
akongstad <kongstad@outlook.com>
2024-11-01 14:42:11 +0100

    feat: add alter commands

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 Main experiment file. Run experiment for all data systems (Sqlite, Postgresql, duckdb, snowflake)
 """
 
-import itertools
+import random
 import sqlite3
 from experiment_logger.data_recorder import (
     DataRecorder,
@@ -22,6 +22,7 @@ recorder = DataRecorder()
 sqlite_conn = sqlite3.connect("sqlite.db")
 # PostgreSQL connection
 
+
 def _current_task_loading(query: str):
     """Simulate progress loading in termimal. Writes the following: "--running: {query}..." to terminal. The dots should blink while the query is running.
 
@@ -30,42 +31,118 @@ def _current_task_loading(query: str):
     """
     sys.stdout.write(f"\r--running: {query}")
     sys.stdout.flush()  # Force output to update in terminal
-    
 
 
-def create_tables(conn,*, database_system: DatabaseSystem, num_objects: Granularity):
+def create_tables(conn, *, database_system: DatabaseSystem, num_objects: Granularity):
     """Example: Create 1000 tables"""
     with conn:
-        for num_exp in range(3):
-            print()
-            logging.info(f"Repetition {num_exp}")
-            for i in range(num_objects.value):
-                _current_task_loading(f"CREATE TABLE t_{i} (id INTEGER PRIMARY KEY, value TEXT);")
+        print()
+        for i in range(num_objects.value):
+            _current_task_loading(
+                f"CREATE TABLE t_{i} (id INTEGER PRIMARY KEY, value TEXT);"
+            )
+
+            start_time = datetime.datetime.now()
+            conn.execute(
+                f"CREATE TABLE t_{i} (id INTEGER PRIMARY KEY, value TEXT);"
+            )
+            conn.commit()
+            end_time = datetime.datetime.now()
+
+            query_time = end_time - start_time
+            record = (
+                database_system,
+                DDLCommand.CREATE,
+                f"CREATE TABLE t_{i} (id INTEGER PRIMARY KEY, value TEXT);",
+                DatabaseObject.TABLE,
+                num_objects,
+                0,
+                query_time.total_seconds(),
+                start_time,
+                end_time,
+            )
+            recorder.record(*record)
                 
-                start_time = datetime.datetime.now()
-                conn.execute(f"CREATE TABLE t_{i}_{num_exp} (id INTEGER PRIMARY KEY, value TEXT);")
-                conn.commit()
-                end_time = datetime.datetime.now()
                 
-                query_time = end_time - start_time
-                record = (
-                    database_system,
-                    DDLCommand.CREATE,
-                    f"CREATE TABLE t_{i} (id INTEGER PRIMARY KEY, value TEXT);",
-                    DatabaseObject.TABLE,
-                    num_objects,
-                    num_exp,
-                    query_time.total_seconds(),
-                    start_time,
-                    end_time,
-                )
-                recorder.record(*record)
     print()
 
 
-def alter_table(conn, object_type: DatabaseObject):
-    """Example: alter table t_0 add column a"""
-    pass
+def alter_tables(conn, database_system: DatabaseSystem, granularity: Granularity):
+    """Example: alter table t_0 add column a. Point query"""
+    table_num = random.randint(0, granularity.value-1)  # In case of prefetching
+    with conn:
+        for num_exp in range(3):
+            print()
+            query = f"ALTER TABLE t_{table_num} ADD COLUMN altered_{num_exp} TEXT;"
+            _current_task_loading(query)
+            start_time = datetime.datetime.now()
+            conn.execute(query)
+            conn.commit()
+            end_time = datetime.datetime.now()
+
+            query_time = end_time - start_time
+            record = (
+                database_system,
+                DDLCommand.ALTER,
+                query,
+                DatabaseObject.TABLE,
+                granularity,
+                num_exp,
+                query_time.total_seconds(),
+                start_time,
+                end_time,
+            )
+            recorder.record(*record)
+            print()
+            comment_object(conn, database_system, DatabaseObject.TABLE, granularity, num_exp)
+            
+
+            
+    print()
+
+
+def comment_object(
+    conn,
+    database_system: DatabaseSystem,
+    database_object: DatabaseObject,
+    granularity: Granularity,
+    num_exp: int,
+):
+    """Example if comment supported: alter table t1 set comment = 'This table has been altered'\n
+    Example if comment not supported: alter table t1 rename to t1_altered"""
+
+    object_num = random.randint(0, granularity.value-1)
+    with conn:
+        if database_system == DatabaseSystem.SQLITE and database_object.value=="table":
+            # ALTER column name
+            query = f"alter table t_{object_num} RENAME COLUMN value TO value_altered;"
+        else:
+            query = f"alter {database_object.value[0].lower()}_{object_num} set comment 'This table has been altered';"
+        _current_task_loading(query)
+        start_time = datetime.datetime.now()
+        conn.execute(query)
+        conn.commit()
+        end_time = datetime.datetime.now()
+
+        query_time = end_time - start_time
+        record = (
+            database_system,
+            DDLCommand.COMMENT,
+            query,
+            DatabaseObject.TABLE,
+            granularity,
+            num_exp,
+            query_time.total_seconds(),
+            start_time,
+            end_time,
+        )
+        recorder.record(*record)
+        
+        # Clean up. For sqlite
+        if database_system == DatabaseSystem.SQLITE and database_object.value=="table":
+            query = f"alter table t_{object_num} RENAME COLUMN value_altered TO value;"
+            conn.execute(query)
+            conn.commit()
 
 
 def show_objects():
@@ -84,22 +161,22 @@ def drop_schema(database_system: DatabaseSystem):
     if database_system == DatabaseSystem.SQLITE:
         if os.path.exists("sqlite.db"):
             os.remove("sqlite.db")
-            logging.info(f"Dropped: {database_system}" )
+            logging.info(f"Dropped: {database_system}")
     else:
         logging.error("Database system not dropped")
-
-    
-    
 
 
 def experiment_1(conn, database_system: DatabaseSystem):
     try:
         logging.info("Starting experiment 1!")
-        
+
         for gran in Granularity:
-            logging.info(f"Experiment: 1 | Object: {DatabaseObject.TABLE} | Granularity: {gran.value} | Status: started")
+            logging.info(
+                f"Experiment: 1 | Object: {DatabaseObject.TABLE} | Granularity: {gran.value} | Status: started"
+            )
             create_tables(conn, database_system=database_system, num_objects=gran)
-            logging.info(f"Experiment: 1 | Object: {DatabaseObject.TABLE} | Granularity: {gran.value} | Status: succesful")
+            alter_tables(conn, database_system, gran)
+
             drop_schema(database_system)
             if database_system == DatabaseSystem.SQLITE:
                 conn = sqlite3.connect("sqlite.db")
@@ -107,11 +184,12 @@ def experiment_1(conn, database_system: DatabaseSystem):
         logging.info("Experiment 1 finished.")
         drop_schema(database_system)
         recorder.close()
-        
-    
-    
+
+
 def main():
-    logging.basicConfig(format='%(levelname)s%(funcName)20s():%(message)s', level=logging.INFO)
+    logging.basicConfig(
+        format="%(levelname)s%(funcName)20s():%(message)s", level=logging.INFO
+    )
     experiment_1(sqlite_conn, DatabaseSystem.SQLITE)
     logging.info("Done!")
 
